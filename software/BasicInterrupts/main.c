247a239ed0813a65f392775a3ce5029a5fdb0919
neoadiatma <166360917+neoadiatma@users.noreply.github.com>
2025-03-25 14:55:45 +1100

    Update main.c
    change counter logic to use decrement and increment of 2

--- software/BasicInterrupts/main.c
+++ software/BasicInterrupts/main.c
@@ -28,9 +28,11 @@ int main(void)
     	}
     	if (flag_up){
     		counter++;
+    		counter++;
     		flag_up = 0; //reset
     	}
     	if (flag_down){
+    		counter--;
     		counter--;
     		flag_down = 0; // reset
     	}
